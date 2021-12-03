f0798247d8da4f024c41fe961eeff26f86bb312a
moravec.tomas <moravec.tomas@ssakhk.cz>
2021-12-03 11:42:32 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BrainFuck_Compiler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
